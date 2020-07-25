b581231e5acff37dcd80c48c141cf1946d8d7ef4
Ziyen <zyloh89@gmail.com>
2020-07-25 11:33:27 +0930

    initial commit

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    Quote: require("./quote"),
+    User: require("./user")
+  };
---
+++ models/quote.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const quoteSchema = new Schema({
+  productType: { 
+      type: String, 
+      required: true 
+    },
+  numberOfGuest: { 
+      type: String, 
+      required: true 
+    },
+  eventType: { 
+      type: String, 
+      required: true 
+    },
+  cakeFlavour: { 
+      type: String, 
+      required: true 
+    },
+  cakeFilling: { 
+      type: String, 
+      required: true 
+    },
+  cakeSize: { 
+      type: String, 
+      required: true 
+    },
+  datePickup: { 
+      type: String, 
+      required: true 
+    },
+  numberOfGuest: { 
+      type: String, 
+      required: true 
+    },
+  otherInfo: { 
+      type: String
+    },
+  createdAt: { 
+      type: Date, 
+      default: Date.now 
+    },
+  user: { 
+      type: Schema.Types.ObjectId, 
+      ref: 'User' 
+    }
+});
+
+const Quote = mongoose.model("Quote", quoteSchema);
+
+module.exports = Quote;
