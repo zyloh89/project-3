65ff59fb6892c5262cd86c0033f262695036f35f
Ziyen <zyloh89@gmail.com>
2020-08-12 20:09:28 +0930

    debug

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { login } = require('../controllers/usercontroller')
+const { login } = require('../controllers/userController')
 
 
 router.post('/login', login)
